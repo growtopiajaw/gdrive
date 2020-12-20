40aad7b16c048985df0dd9edc267628924a69fbd
Growtopia Jaw <growtopiajaw@gmail.com>
2020-12-21 00:38:24 +0800

    Pull changes from upstream before building

--- _release/build.sh
+++ _release/build.sh
@@ -8,6 +8,8 @@ package="github.com/GrowtopiaJaw/gdrive"
 package_split=(${package//\// })
 package_name=${package_split[-1]}
 
+go get -u $package
+
 platforms=("aix/ppc64" "darwin/amd64" "dragonfly/amd64" "freebsd/386" "freebsd/amd64" "freebsd/armv6" "freebsd/armv7" "freebsd/arm64" "illumos/amd64" "js/wasm" "linux/386" "linux/amd64" "linux/armv5" "linux/armv6" "linux/armv7" "linux/arm64" "linux/ppc64" "linux/ppc64le" "linux/mips64" "linux/mips64le" "linux/riscv64" "linux/s390x" "netbsd/386" "netbsd/amd64" "netbsd/armv6" "netbsd/armv7" "netbsd/arm64" "openbsd/386" "openbsd/amd64" "openbsd/armv6" "openbsd/armv7" "openbsd/arm64" "plan9/386" "plan9/amd64" "plan9/arm" "solaris/amd64" "windows/386" "windows/amd64" "windows/arm")
 
 # Unsupported architectures
