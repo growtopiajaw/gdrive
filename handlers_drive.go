7fcaade1a633e0f7496ec15bbfb9f7c6f8f721dc
Chaskiel Grundman <cgrundman@gmail.com>
2022-06-19 22:05:05 -0400

    Add support for OAuth callback flow
    Squash of the following commits:
    
    Add oauth redirect listener
    
    Add an HTTP listener for use as the redirect_uri. This will allow gdrive
    to work after google phases out the "oob" redirect.
    Also implement oauth PKCE (code challenge)
    
    (cherry picked from commit ab31f427b540aa287d208aecfeadbda41f4aa3a6)
    
    Remove debug prints from oauth code
    
    (cherry picked from commit 1c4005f78434d703331b9c00864abfd302b742d5)
    
    Finish renaming redirect to callback
    
    The redirect_uri is where the google server redirects after completing
    authentication.
    From our perspective, it's a callback, not a redirect.
    Our authorize uri redirects to google, so it would be confusing
    to refer to the other url as a redirect
    
    (cherry picked from commit f2bd11a969a2d57a1bd6f4999f559a46a0d85d98)

--- handlers_drive.go
+++ handlers_drive.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -408,7 +407,7 @@ func getOauthClient(args cli.Arguments) (*http.Client, error) {
 	}
 
 	tokenPath := ConfigFilePath(configDir, TokenFilename)
-	return auth.NewFileSourceClient(clientId, clientSecret, tokenPath, authCodePrompt)
+	return auth.NewFileSourceClient(clientId, clientSecret, tokenPath, auth.AuthCodeHTTP)
 }
 
 func getConfigDir(args cli.Arguments) string {
@@ -431,21 +430,6 @@ func newDrive(args cli.Arguments) *drive.Drive {
 	}
 
 	return client
-}
-
-func authCodePrompt(url string) func() string {
-	return func() string {
-		fmt.Println("Authentication needed")
-		fmt.Println("Go to the following url in your browser:")
-		fmt.Printf("%s\n\n", url)
-		fmt.Print("Enter verification code: ")
-
-		var code string
-		if _, err := fmt.Scan(&code); err != nil {
-			fmt.Printf("Failed reading code: %s", err.Error())
-		}
-		return code
-	}
 }
 
 func progressWriter(discard bool) io.Writer {
